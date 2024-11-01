8c7f4a0ff4905887954d149f43a9d7db81d0ad7f
YokotaYoshi <ymdtr5274@gmail.com>
2024-11-01 22:00:23 +0900

    oo

--- Soukoban/Assets/Scripts/PlayerController.cs
+++ Soukoban/Assets/Scripts/PlayerController.cs
@@ -13,9 +13,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
-           
+           transform.Translate(new Vector3(0,1,0) * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+           transform.position -= new Vector3(0,1,0);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+           transform.position += new Vector3(1,0,0);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+           transform.position -= new Vector3(1,0,0);
         }
     }
 }
