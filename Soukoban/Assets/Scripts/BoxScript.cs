02d86ddbc49b564c5a960014fa968bfbb400e99d
YokotaYoshi <ymdtr5274@gmail.com>
2024-11-06 23:00:50 +0900

    o

--- Soukoban/Assets/Scripts/BoxScript.cs
+++ Soukoban/Assets/Scripts/BoxScript.cs
@@ -31,7 +31,7 @@ public class BoxScript : MonoBehaviour
         Vector3 left = Distance - Vector3.left;
         Vector3 right = Distance - Vector3.right;
         InputStay += Time.deltaTime;
-        if (Distance.magnitude < 1.1f)
+        if (Distance.magnitude < 1.01f)
         {
             rb2d.constraints &= ~RigidbodyConstraints2D.FreezePositionX;
             rb2d.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
