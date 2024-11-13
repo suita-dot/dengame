dee491a9a7e6b768dcff0bb3211b7cd5ae3ac6e2
YoshiakiYokota96 <137075526+YoshiakiYokota96@users.noreply.github.com>
2024-11-13 13:58:07 +0900

    go

--- Soukoban/Assets/Scripts/LaserScript.cs
+++ Soukoban/Assets/Scripts/LaserScript.cs
@@ -6,6 +6,10 @@ public class LaserScript : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private float speed = 5f;
+    public bool isRight = false;
+    public bool isLeft = false;
+    public bool isUp = false;
+    public bool isDown = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,22 @@ public class LaserScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb2d.velocity = new Vector3(speed, 0,0);
+        if (isRight)
+        {
+            rb2d.velocity = new Vector3(speed, 0,0);
+        }
+        if (isLeft)
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (isUp)
+        {
+            rb2d.velocity = new Vector3(0, speed,0);
+        }
+        if (isDown)
+        {
+            rb2d.velocity = new Vector3(0, speed,0);
+        }        
     }
 
     void OnTriggerEnter2D(Collider2D other)
