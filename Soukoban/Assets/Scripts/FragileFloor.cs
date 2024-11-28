9bee34b955e166a03ca11c89949024940c188987
YokotaYoshi <ymdtr5274@gmail.com>
2024-11-28 18:03:38 +0900

    1

--- Soukoban/Assets/Scripts/FragileFloor.cs
+++ Soukoban/Assets/Scripts/FragileFloor.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class FragileFloor : MonoBehaviour
 {
+    //BoxCollider2Dを取得その1
     private BoxCollider2D stopper;
+    //SpriteRendererを取得
     public SpriteRenderer floorRenderer;
+    //壊れた床のSpriteを取得
     public Sprite broken;
     // Start is called before the first frame update
     void Start()
     {
+        //BoxCollider2Dを取得その2
         stopper = GetComponent<BoxCollider2D>();
     }
 
+    //プレイヤーがすり抜けて出て行った場合に、すり抜けられなくしてかつ画像を壊れた床に変更
     void OnTriggerExit2D(Collider2D other)
     {
+        //もし出て行ったgameObjectのタグが”Player”だったら
         if (other.gameObject.tag == "Player")
         {
+            //BoxCollider2DのisTriggerをfalseにしてすり抜けられないようにする
             stopper.isTrigger = false;
+            //SpriteRendererのspriteを壊れた床の画像にする。
             floorRenderer.sprite = broken;
         }
     }
