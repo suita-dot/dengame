bb05530b6cc5b1af5b4e4bfa36ef4643c2fac22a
YokotaYoshi <ymdtr5274@gmail.com>
2024-11-30 14:45:12 +0900

    d

--- UniTopGame/Assets/Scripts/CameraManager.cs
+++ UniTopGame/Assets/Scripts/CameraManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    public GameObject otherTarget;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,16 @@ public class CameraManager : MonoBehaviour
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         if (player != null)
         {
-            transform.position = new Vector3(player.transform.position.x,player.transform.position.y, -10);
+            if (otherTarget != null)
+            {
+                Vector2 pos = Vector2.Lerp(player.transform.position,otherTarget.transform.position, 0.5f);
+                transform.position = new Vector3(pos.x, pos.y, -10);
+            }
+            else
+            {
+                transform.position = new Vector3(player.transform.position.x,player.transform.position.y, -10);
+            }
+            
         }
 
     }
