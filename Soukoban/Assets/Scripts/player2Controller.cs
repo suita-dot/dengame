8fff698a7758081d9217bc17f043b97555deed36
YoshiakiYokota96 <137075526+YoshiakiYokota96@users.noreply.github.com>
2024-12-09 13:15:25 +0900

    1

--- Soukoban/Assets/Scripts/player2Controller.cs
+++ Soukoban/Assets/Scripts/player2Controller.cs
@@ -29,9 +29,33 @@ public class player2Controller : MonoBehaviour
         {
             rb2d.velocity = Vector3.up *speed;            
         }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb2d.velocity = Vector3.down *speed;            
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb2d.velocity = Vector3.right *speed;            
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.velocity = Vector3.left *speed;            
+        }
         if (Input.GetKeyUp(KeyCode.UpArrow))
         {
             StartCoroutine(Up());
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            StartCoroutine(Down());
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            StartCoroutine(Right());
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            StartCoroutine(Left());
         }
         
     }
@@ -39,6 +63,48 @@ public class player2Controller : MonoBehaviour
     {
         Vector3 targetPosition = new Vector3 (transform.position.x, Mathf.Ceil(transform.position.y), transform.position.z);
         float gap = Mathf.Ceil(transform.position.y)-transform.position.y;
+        float estimateTime = gap/speed;
+        float elapsedTime = 0f;
+        while(estimateTime >= elapsedTime)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        rb2d.velocity = Vector2.zero;
+        transform.position = new Vector3(Mathf.Round(targetPosition.x),Mathf.Round(targetPosition.y),Mathf.Round(targetPosition.z));
+    }
+    private IEnumerator Down()
+    {
+        Vector3 targetPosition = new Vector3 (transform.position.x, Mathf.Floor(transform.position.y), transform.position.z);
+        float gap = -Mathf.Floor(transform.position.y)+transform.position.y;
+        float estimateTime = gap/speed;
+        float elapsedTime = 0f;
+        while(estimateTime >= elapsedTime)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        rb2d.velocity = Vector2.zero;
+        transform.position = new Vector3(Mathf.Round(targetPosition.x),Mathf.Round(targetPosition.y),Mathf.Round(targetPosition.z));
+    }
+    private IEnumerator Right()
+    {
+        Vector3 targetPosition = new Vector3 (Mathf.Ceil(transform.position.x), transform.position.y, transform.position.z);
+        float gap = Mathf.Ceil(transform.position.x)-transform.position.x;
+        float estimateTime = gap/speed;
+        float elapsedTime = 0f;
+        while(estimateTime >= elapsedTime)
+        {
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        rb2d.velocity = Vector2.zero;
+        transform.position = new Vector3(Mathf.Round(targetPosition.x),Mathf.Round(targetPosition.y),Mathf.Round(targetPosition.z));
+    }
+    private IEnumerator Left()
+    {
+        Vector3 targetPosition = new Vector3 (Mathf.Floor(transform.position.x), transform.position.y, transform.position.z);
+        float gap = -Mathf.Floor(transform.position.x)+transform.position.x;
         float estimateTime = gap/speed;
         float elapsedTime = 0f;
         while(estimateTime >= elapsedTime)
